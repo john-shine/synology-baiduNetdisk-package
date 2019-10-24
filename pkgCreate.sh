b84e0b30f53d29ca2527d3f403d9bbea905b256d
john <john@JohndeAir>
2019-10-24 16:51:26 +0800

    compatibility for DSM5.2

--- pkgCreate.sh
+++ pkgCreate.sh
@@ -3,5 +3,5 @@
 tar -czf package.tgz ./package/*
 mkdir -p build/
 rm -f build/baiduNetdisk.spk
-tar -cvf build/baiduNetdisk.spk scripts/* INFO PACKAGE_ICON.PNG PACKAGE_ICON_256.PNG LICENSE package.tgz
+tar -cvf build/baiduNetdisk.spk scripts/* INFO PACKAGE_ICON.PNG PACKAGE_ICON_256.PNG LICENSE package.tgz conf/
 rm -f package.tgz
