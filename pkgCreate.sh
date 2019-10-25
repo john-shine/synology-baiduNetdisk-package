8b39b86c62b957c775be387c139b4e191d1787c2
john <john@JohndeAir>
2019-10-25 16:57:34 +0800

    fix package restart

--- pkgCreate.sh
+++ pkgCreate.sh
@@ -2,6 +2,7 @@
 
 tar -czf package.tgz ./package/*
 mkdir -p build/
+mkdir -p conf/
 rm -f build/baiduNetdisk.spk
-tar -cvf build/baiduNetdisk.spk scripts/* INFO PACKAGE_ICON.PNG PACKAGE_ICON_256.PNG LICENSE package.tgz conf/
+tar -cvf build/baiduNetdisk.spk scripts/* INFO PACKAGE_ICON.PNG PACKAGE_ICON_256.PNG package.tgz conf/
 rm -f package.tgz
