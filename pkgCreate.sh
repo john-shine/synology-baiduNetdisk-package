fa6f3bbdee0afc6abbb1cb22f8e2b710dbe63cd5
john-shine <mr.john.shine@gmail.com>
2019-11-02 23:57:31 +0800

    to version 1.0.1

--- pkgCreate.sh
+++ pkgCreate.sh
@@ -4,9 +4,9 @@ pkg_version=$(cat VERSION)
 cp INFO .INFO
 echo -e "\nversion=\"${pkg_version}\"" >> INFO
 tar -czf package.tgz ./package/*
+rm -rf build/
 mkdir -p build/
 mkdir -p conf/
-rm -f build/baiduNetdisk.spk
 tar -cvf "build/baiduNetdisk-${pkg_version}.spk" scripts/* INFO PACKAGE_ICON.PNG PACKAGE_ICON_256.PNG package.tgz conf/
 rm -f package.tgz
 mv .INFO INFO
