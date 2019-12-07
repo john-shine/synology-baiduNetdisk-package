2ac12b9856f115141a893496a4b7105ef83fc907
johnshine <mr.john.shine@gmail.com>
2019-12-07 15:41:41 +0800

    update version to 1.0.2

--- pkgCreate.sh
+++ pkgCreate.sh
@@ -3,10 +3,10 @@
 pkg_version=$(cat VERSION)
 cp INFO .INFO
 echo -e "\nversion=\"${pkg_version}\"" >> INFO
-tar -czf package.tgz ./package/*
+tar -czf package.tgz --owner=0 --group=0 ./package/*
 rm -rf build/
 mkdir -p build/
 mkdir -p conf/
-tar -cvf "build/baiduNetdisk-${pkg_version}.spk" scripts/* INFO PACKAGE_ICON.PNG PACKAGE_ICON_256.PNG package.tgz conf/
+tar -cvf "build/baiduNetdisk-${pkg_version}.spk" --owner=0 --group=0 scripts/* INFO PACKAGE_ICON.PNG PACKAGE_ICON_256.PNG package.tgz conf/
 rm -f package.tgz
 mv .INFO INFO
